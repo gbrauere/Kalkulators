30711414d0508d7c86516c32a20db702db6b9055
gbrauere <145644371+gbrauere@users.noreply.github.com>
2024-05-16 09:36:09 +0300

    5

--- kalkulators.py
+++ kalkulators.py
@@ -36,46 +36,63 @@ def Vienads():
     e.insert(0,str(result))
     return 0
 
-command=lambda:btnCommand('-')
-command=lambda:btnCommand('+')
-command=lambda:btnCommand('*')
-command=lambda:btnCommand('/')
+def notirit():
+    e.delete(0,END)
+    num1=0
+    mathOp=""
+    return 0
 
+e=Entry(mansLogs,width=18,bd=10,font=("Arial Black",20),justify="right",bg='#b0ebb2') #izveido ievades logu/displeju
 
-e=Entry(mansLogs,width=15,bd=10,font=("Arial Black",20),justify="right") #izveido ievades logu/displeju
+btn0=Button(mansLogs,text='0',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(0),bg='#4dd652'
+) #izveido pogu
+btn1=Button(mansLogs,text='1',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(1),bg='#4dd652'
+)
+btn2=Button(mansLogs,text='2',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(2),bg='#4dd652'
+)
+btn3=Button(mansLogs,text='3',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(3),bg='#4dd652'
+)
+btn4=Button(mansLogs,text='4',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(4),bg='#4dd652'
+)
+btn5=Button(mansLogs,text='5',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(5),bg='#4dd652'
+)
+btn6=Button(mansLogs,text='6',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(6),bg='#4dd652'
+)
+btn7=Button(mansLogs,text='7',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(7),bg='#4dd652'
+)
+btn8=Button(mansLogs,text='8',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(8),bg='#4dd652'
+)
+btn9=Button(mansLogs,text='9',padx='35',pady='20',bd='5',font=("Lucida Sans Typewriter",10),command=lambda:btnClick(9),bg='#4dd652'
+)
 
-btn0=Button(mansLogs,text='0',padx='40',pady='20') #izveido pogu
-btn1=Button(mansLogs,text='1',padx='40',pady='20')
-btn2=Button(mansLogs,text='2',padx='40',pady='20')
-btn3=Button(mansLogs,text='3',padx='40',pady='20')
-btn4=Button(mansLogs,text='4',padx='40',pady='20')
-btn5=Button(mansLogs,text='5',padx='40',pady='20')
-btn6=Button(mansLogs,text='6',padx='40',pady='20')
-btn7=Button(mansLogs,text='7',padx='40',pady='20')
-btn8=Button(mansLogs,text='8',padx='40',pady='20')
-btn9=Button(mansLogs,text='9',padx='40',pady='20')
+btnSask=Button(mansLogs,text='+',padx='35',pady='20',bd='5',command=lambda:btnCommand('+'),bg='green',font=("Lucida Sans Typewriter",10))
+btnAtn=Button(mansLogs,text='-',padx='35',pady='20',bd='5',command=lambda:btnCommand('-'),bg='green',font=("Lucida Sans Typewriter",10))
+btnDal=Button(mansLogs,text='/',padx='35',pady='20',bd='5',command=lambda:btnCommand('/'),bg='green',font=("Lucida Sans Typewriter",10))
+btnReiz=Button(mansLogs,text='*',padx='35',pady='20',bd='5',command=lambda:btnCommand('*'),bg='green',font=("Lucida Sans Typewriter",10))
+btnC=Button(mansLogs,text='C',padx='35',pady='20',bd='5',command=notirit,bg='green',font=("Lucida Sans Typewriter",10))
+btnVien=Button(mansLogs,text='=',padx='35',pady='20',bd='5',command=Vienads,bg='green',font=("Lucida Sans Typewriter",10))
 
-btnSask=Button(mansLogs,text='+',padx='40',pady='20')
-btnAtn=Button(mansLogs,text='-',padx='40',pady='20')
-btnC=Button(mansLogs,text='C',padx='40',pady='20')
+e.grid(row=0,column=0,columnspan=4,) #displeja atrašanās vieta
 
-e.grid(row=0,column=0,columnspan=4) #displeja atrašanās vieta
+btnSask.grid(row=2,column=3)
+btnAtn.grid(row=3,column=3)
+btnDal.grid(row=4,column=3)
+btnReiz.grid(row=5,column=3)
+btnC.grid(row=5,column=1)
+btnVien.grid(row=5,column=2)
 
-btnSask.grid(row=3,column=3)
-btnC.grid(row=1,column=3)
+btn7.grid(row=2,column=0)
+btn8.grid(row=2,column=1)
+btn9.grid(row=2,column=2)
 
-btn7.grid(row=1,column=0)
-btn8.grid(row=1,column=1)
-btn9.grid(row=1,column=2)
+btn4.grid(row=3,column=0)
+btn5.grid(row=3,column=1)
+btn6.grid(row=3,column=2)
 
-btn4.grid(row=2,column=0)
-btn5.grid(row=2,column=1)
-btn6.grid(row=2,column=2)
+btn1.grid(row=4,column=0)
+btn2.grid(row=4,column=1)
+btn3.grid(row=4,column=2)
 
-btn1.grid(row=3,column=0)
-btn2.grid(row=3,column=1)
-btn3.grid(row=3,column=2)
-
-btn0.grid(row=4,column=0)
+btn0.grid(row=5,column=0)
 
 mansLogs.mainloop()
